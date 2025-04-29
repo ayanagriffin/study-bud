1bbfcbc279077bc01c9bb50070921ff8be7c3517
Ayana Griffin <ayanajg@gmail.com>
2025-04-28 17:13:55 -0700

    using modifiers

--- StudyBud/Components/MainButton.swift
+++ StudyBud/Components/MainButton.swift
@@ -11,22 +11,11 @@ struct MainButton: View {
     var title: String
     var action: () -> Void
 
-    @State private var isPressed = false
-
     var body: some View {
         Button(action: action) {
             Text(title)
-                .font(.system(size: 20, weight: .bold))
-                .foregroundColor(.black)
-                .frame(maxWidth: .infinity, minHeight: 50)
-                .background(isPressed ? Color.buttonSecodary : Color.buttonPrimary)
-                .cornerRadius(CornerRadius.button)
-                .padding(.horizontal, 20)
+                .font(.buttonText)
+                .mainButtonStyle()
         }
-        .buttonStyle(PlainButtonStyle())
-        .simultaneousGesture(DragGesture(minimumDistance: 0)
-            .onChanged { _ in isPressed = true }
-            .onEnded { _ in isPressed = false }
-        )
     }
 }
